bba06b12e7da59c086c45bfaeb658ae189b059dc
Matthias <26800596+not-matthias@users.noreply.github.com>
2021-12-29 01:28:03 +0100

    Replaced bitfield NPT with x86 structures

--- driver/src/svm/data/nested_page_table.rs
+++ driver/src/svm/data/nested_page_table.rs
@@ -1,21 +1,21 @@
 use crate::nt::addresses::physical_address;
+use crate::nt::memory::AllocatedMemory;
+use crate::svm::paging::PFN_MASK;
 use elain::Align;
 use x86::bits64::paging::{
-    PAddr, PDEntry, PDFlags, PDPTEntry, PDPTFlags, PML4Entry, PML4Flags, MAXPHYADDR,
+    PAddr, PDEntry, PDFlags, PDPTEntry, PDPTFlags, PML4Entry, PML4Flags, PAGE_SIZE_ENTRIES, PD,
+    PDPT,
 };
-
-use crate::nt::memory::AllocatedMemory;
-use crate::svm::paging::{page_to_pfn, LegacyPDE, LegacyPDPTE, LegacyPML4E};
 
 #[repr(C, align(4096))]
 pub struct NestedPageTable {
-    pub pml4: [LegacyPML4E; 1],
+    pub pml4: [PML4Entry; 1],
     align_0: Align<4096>,
 
-    pub pdp_entries: [LegacyPDPTE; 512],
+    pub pdp_entries: PDPT,
     align_1: Align<4096>,
 
-    pub pd_entries: [[LegacyPDE; 512]; 512],
+    pub pd_entries: [PD; 512],
     align_2: Align<4096>,
 }
 
@@ -31,74 +31,34 @@ impl NestedPageTable {
 
         // PML4
         //
+        (*self.ptr()).pml4[0] = PML4Entry::new(
+            physical_address((*self.ptr()).pdp_entries.as_ptr() as _),
+            PML4Flags::from_iter([PML4Flags::P, PML4Flags::RW, PML4Flags::US]),
+        );
 
-        let pa = physical_address((*self.ptr()).pdp_entries.as_ptr() as _);
-
-        let flags = PML4Flags::from_iter([PML4Flags::P, PML4Flags::RW, PML4Flags::US]);
-        let pml4 = PML4Entry::new(pa, flags);
-
-        let mut legacy = LegacyPML4E(0);
-        legacy.set_page_frame_number(page_to_pfn!(pa));
-        legacy.set_valid(1);
-        legacy.set_write(1);
-        legacy.set_user(1);
-
-        if legacy.0 != pml4.0 {
-            log::error!("PML4Entry: {:?} != {:?}", legacy.0, pml4.0);
-        }
-        // assert_eq!(legacy.0, pml4.0);
-
-        (*self.ptr()).pml4[0] = legacy.into();
-
-        // PDPTE
+        // PDPT
         //
-        for (i, table_pdp) in (*self.ptr()).pdp_entries.iter_mut().enumerate() {
+        for (i, pdp) in (*self.ptr()).pdp_entries.iter_mut().enumerate() {
             let pa = physical_address((*self.ptr()).pd_entries[i].as_ptr() as _);
+            *pdp = PDPTEntry::new(
+                pa,
+                PDPTFlags::from_iter([PDPTFlags::P, PDPTFlags::RW, PDPTFlags::US]),
+            );
 
-            let flags = PDPTFlags::from_iter([PDPTFlags::P, PDPTFlags::RW, PDPTFlags::US]);
-            let pdp = PDPTEntry::new(pa, flags);
-
-            let mut legacy_pdp = LegacyPML4E(0);
-            legacy_pdp.set_page_frame_number(page_to_pfn!(pa));
-            legacy_pdp.set_valid(1);
-            legacy_pdp.set_write(1);
-            legacy_pdp.set_user(1);
-
-            if legacy_pdp.0 != pdp.0 {
-                log::error!("PDPTEntry: {:?} != {:?}", legacy_pdp.0, pdp.0);
-            }
-            // assert_eq!(legacy_pdp.0, pdp.0);
-
-            *table_pdp = legacy_pdp;
-
-            // PDE
+            // PD
             //
-            for (j, table_pd) in (*self.ptr()).pd_entries[i].iter_mut().enumerate() {
-                let pa = i * (*self.ptr()).pd_entries[i].len() + j;
-                let pa = pa as u64;
+            for (j, pd) in (*self.ptr()).pd_entries[i].iter_mut().enumerate() {
+                let pa = (i * PAGE_SIZE_ENTRIES + j) as u64;
 
                 // Mask to find the page frame number. We have to use this so that we can use `PDEntry`.
                 // This is the same as using a `bitflags` struct and setting the bits `21-51`.
                 //
-                const ADDRESS_MASK: u64 = ((1 << MAXPHYADDR) - 1) & !0xfff;
-                let pfn = pa << 21 & ADDRESS_MASK;
+                let pfn = pa << 21 & PFN_MASK;
 
-                let flags = PDFlags::from_iter([PDFlags::P, PDFlags::RW, PDFlags::US, PDFlags::PS]);
-                let pd = PDEntry::new(PAddr::from(pfn), flags);
-
-                let mut legacy_pd = LegacyPDE(0);
-                legacy_pd.set_page_frame_number(pa);
-                legacy_pd.set_valid(1);
-                legacy_pd.set_write(1);
-                legacy_pd.set_user(1);
-                legacy_pd.set_large_page(1);
-
-                if legacy_pd.0 != pd.0 {
-                    log::error!("PDEntry({:x?}): {:?} != {:?}", pa, legacy_pd.0, pd.0);
-                }
-                // assert_eq!(legacy_pd.0, pd.0);
-
-                *table_pd = legacy_pd;
+                *pd = PDEntry::new(
+                    PAddr::from(pfn),
+                    PDFlags::from_iter([PDFlags::P, PDFlags::RW, PDFlags::US, PDFlags::PS]),
+                );
             }
         }
 
--- driver/src/svm/paging.rs
+++ driver/src/svm/paging.rs
@@ -1,8 +1,9 @@
-use bitfield::bitfield;
+use x86::bits64::paging::MAXPHYADDR;
 
 pub const PAGE_SHIFT: u64 = 12;
 pub const PAGE_SIZE: usize = 0x1000;
 pub const PAGE_MASK: usize = !(PAGE_SIZE - 1);
+pub const PFN_MASK: u64 = ((1 << MAXPHYADDR) - 1) & !0xfff;
 
 /// Converts a page address to a page frame number.
 pub macro page_to_pfn($page: expr) {
@@ -21,42 +22,4 @@ pub macro page_to_pfn($page: expr) {
 /// // See: https://stackoverflow.com/questions/20771394/how-to-understand-the-macro-of-page-align-in-kernel/20771666
 pub macro page_align($virtual_address:expr) {
     ($virtual_address + crate::svm::paging::PAGE_SIZE - 1) & crate::svm::paging::PAGE_MASK
-}
-
-pub type LegacyPDPTE = LegacyPML4E;
-
-bitfield! {
-    /// See Figure 5-25. (2-Mbyte PDE—Long Mode)
-    pub struct LegacyPDE(u64);
-    pub get_valid, set_valid: 0, 0;                             // [0]
-    pub get_write, set_write: 1, 1;                             // [1]
-    pub get_user, set_user: 2, 2;                               // [2]
-    pub get_write_through, set_write_through: 3, 3;             // [3]
-    pub get_cache_disable, set_cache_disable: 4, 4;             // [4]
-    pub get_accessed, set_accessed: 5, 5;                       // [5]
-    pub get_dirty, set_dirty: 6, 6;                             // [6]
-    pub get_large_page, set_large_page: 7, 7;                   // [7]
-    pub get_global, set_global: 8, 8;                           // [8]
-    pub get_avl, set_avl: 11, 9;                                // [9-11]
-    pub get_pat, set_pat: 12, 12;                               // [12]
-    // reserved                                                 // [13-20]
-    pub get_page_frame_number, set_page_frame_number: 51, 21;   // [21-51]
-    // reserved                                                 // [52-62]
-    pub get_no_execute, set_no_execute: 63, 63;                 // [63]
-}
-
-bitfield! {
-    /// See Figure 5-23. (2-Mbyte PML4E—Long Mode)
-    pub struct LegacyPML4E(u64);
-    pub get_valid, set_valid: 0, 0;                             // [0]
-    pub get_write, set_write: 1, 1;                             // [1]
-    pub get_user, set_user: 2, 2;                               // [2]
-    pub get_write_through, set_write_through: 3, 3;             // [3]
-    pub get_cache_disable, set_cache_disable: 4, 4;             // [4]
-    pub get_accessed, set_accessed: 5, 5;                       // [5]
-    // reserved                                                 // [6-8]
-    pub get_avl, set_avl: 11, 9;                                // [9-11]
-    pub get_page_frame_number, set_page_frame_number: 51, 12;   // [12-51]
-    // reserved                                                 // [52-62]
-    pub get_no_execute, set_no_execute: 63, 63;                 // [63]
 }
