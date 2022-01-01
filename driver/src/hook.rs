7e3f8b7d911384414f740538b134be21e128a66f
Matthias <26800596+not-matthias@users.noreply.github.com>
2022-01-01 20:39:02 +0100

    Worked on HookedNpt: Implemented initialization

--- driver/src/hook.rs
+++ driver/src/hook.rs
@@ -1,30 +1,57 @@
-#[allow(unused)]
-#[allow(dead_code)]
 extern crate alloc;
 
+use crate::nt::memory::AllocatedMemory;
+use crate::svm::data::nested_page_table::NestedPageTable;
+use alloc::string::String;
 use alloc::vec::Vec;
 use nt::kernel::get_system_routine_address;
 use x86::current::paging::PML4;
 
-pub struct HookedNpt {}
+pub struct Hook {
+    address: usize,
+    handler: *const (),
+
+    page: AllocatedMemory<u8>,
+}
+
+impl Hook {
+    pub fn new(name: String, handler: *const ()) -> Option<Self> {
+        let address = get_system_routine_address(&name)?;
+        log::info!("Found address of {}: {:#x}", &name, address);
+
+        Some(Self {
+            address,
+            handler,
+            page: AllocatedMemory::alloc_contiguous(0x1000)?,
+        })
+    }
+}
+
+pub struct HookedNpt {
+    npt: AllocatedMemory<NestedPageTable>,
+    hooks: Vec<Hook>,
+}
 
 impl HookedNpt {
     pub fn new() -> Option<Self> {
-        Some(Self {})
+        Some(Self {
+            npt: NestedPageTable::identity()?,
+            hooks: Vec::new(),
+        })
     }
 
-    /// Install the hooks on the passed functions.
-    pub fn install(functions: &[&str]) {
-        let _hook_entries = Vec::<u8>::new();
+    /// Hooks the specified function.
+    ///
+    /// ## Parameters
+    ///
+    /// - `function`: The name of the function to hook.
+    /// - `handler`: The function that should be called from the hook.
+    ///
+    pub fn hook(mut self, function: String, handler: *const ()) -> Option<Self> {
+        let hook = Hook::new(function, handler)?;
+        self.hooks.push(hook);
 
-        for &function in functions {
-            let _address = get_system_routine_address(function).unwrap();
-
-            // let hook_entry = HookEntry::new(address as u64, handler);
-
-            // Get a memory resource for install hook on the address.
-            //
-        }
+        Some(self)
     }
 
     pub fn visible() -> bool {
